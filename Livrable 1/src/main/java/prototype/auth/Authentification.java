e007afc5c6dcc2ea96a903dd214663c6b6495606
mathiiias13 <mathias.la.rochelle@umontreal.ca>
2024-10-10 09:53:13 -0400

    Supprime les fichiers .DS_Store et autres changements locaux

---
+++ Livrable 1/src/main/java/prototype/auth/Authentification.java
@@ -0,0 +1,125 @@
+package main.java.prototype.auth;
+
+import main.java.prototype.ApplicationMaVille;
+import main.java.prototype.managers.UtilisateurManager;
+import main.java.prototype.utils.GenerateurCodeDeHachage;
+import main.java.prototype.entities.Intervenant;
+import main.java.prototype.entities.Resident;
+
+import java.util.Scanner;
+
+public class Authentification {
+
+    private final ApplicationMaVille app;
+    private static final String CHEMIN_ABSOLU_RESIDENT = "/Users/mathiaslarochelle/Documents/School/A24/IFT2255/Projets/Projet 1/Livrable 1/ressources/data/resident_data.json";
+    private static final String CHEMIN_ABSOLU_INTERVENANT = "/Users/mathiaslarochelle/Documents/School/A24/IFT2255/Projets/Projet 1/Livrable 1/ressources/data/intervenant_data.json";
+
+    public enum OptionsEnregistrement {
+        ENREGISTRER(1, "S'enregistrer"),
+        CONNECTER(2, "Se connecter");
+
+        private final int valeur;
+        private final String description;
+
+        OptionsEnregistrement(int valeur, String description) {
+            this.valeur = valeur;
+            this.description = description;
+        }
+
+        public int getValeur() {
+            return valeur;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+
+    public Authentification(ApplicationMaVille app) {
+        this.app = app;
+    }
+
+    public boolean traiterChoixAuthentification(int choix) {
+        for (OptionsEnregistrement option : OptionsEnregistrement.values()) {
+            if (option.getValeur() == choix) {
+                return switch (option) {
+                    case ENREGISTRER -> app.gererEnregistrement();
+                    case CONNECTER -> app.gererConnexion();
+                };
+            }
+        }
+        System.out.println("Option invalide. Veuillez réessayer.");
+        return false;
+    }
+
+    public static boolean inscrireResident(Scanner scanner) {
+        System.out.println("Inscription en tant que résident :");
+        try {
+            Resident nouveauResident = new Resident.Builder()
+                    .nom(demanderInfo("Nom complet", scanner))
+                    .dateNaissance(demanderInfo("Date de naissance (DD/MM/YYYY)", scanner))
+                    .email(demanderInfo("Adresse courriel", scanner))
+                    .mdp(demanderInfo("Mot de passe", scanner))
+                    .telephone(demanderInfo("Numéro de téléphone (optionnel)", scanner))
+                    .adresse(demanderInfo("Adresse résidentielle", scanner))
+                    .type("Résident")
+                    .build();
+
+            try {
+                UtilisateurManager manager = new UtilisateurManager();
+                manager.sauvegardeUtilisateurAJSON(nouveauResident, CHEMIN_ABSOLU_RESIDENT);
+            } catch (Exception exception) {
+                System.out.println("Impossible d'enregistrer les informations dans la base de données.");
+                exception.printStackTrace();
+                return false;
+            }
+        } catch (Exception exception) {
+            System.out.println("L'information entrée est invalide. Veuillez réessayer.");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean inscrireIntervenant(Scanner scanner) {
+        System.out.println("Inscription en tant qu'intervenant :");
+        try {
+            Intervenant nouvelIntervenant = new Intervenant.Builder()
+                    .nom(demanderInfo("Nom complet", scanner))
+                    .email(demanderInfo("Adresse courriel", scanner))
+                    .mdp(demanderInfo("Mot de passe", scanner))
+                    .identifiant(demanderInfo("Identifiant de la ville", scanner))
+                    .typeEntreprise(Intervenant.demanderTypeEntreprise(scanner))
+                    .type("Intervenant")
+                    .build();
+
+            try {
+                UtilisateurManager manager = new UtilisateurManager();
+                manager.sauvegardeUtilisateurAJSON(nouvelIntervenant, CHEMIN_ABSOLU_INTERVENANT);
+            } catch (Exception exception) {
+                System.out.println("Impossible d'enregistrer les informations dans la base de données.");
+                exception.printStackTrace();
+                return false;
+            }
+        } catch (Exception exception) {
+            System.out.println("L'information entrée est invalide. Veuillez réessayer.");
+            return false;
+        }
+        return true;
+    }
+
+    private static String demanderInfo(String etiquette, Scanner scanner) {
+        System.out.print(etiquette + " : ");
+        return scanner.nextLine();
+    }
+
+
+    public static String connexionUtilisateur(String nom, String mdp) {
+        int codeDeHachage = GenerateurCodeDeHachage.generationCodeDeHachage(nom, mdp);
+
+        return UtilisateurManager.rechercheUtilisateurDansJSON(codeDeHachage);
+    }
+
+    public static boolean itererTantQueConnexionUtilisateur(String nom, String mdp) {
+        return connexionUtilisateur(nom, mdp) != null;
+    }
+}
